5bf1c68d6a786ac3632060704db110f8cb1868f6
Daniil <89221449094dg@gmail.com>
2025-05-12 23:19:03 +0500

    check length

--- src/main/java/ru/kovrochist/platform/mono/mapper/order/OrderMapper.java
+++ src/main/java/ru/kovrochist/platform/mono/mapper/order/OrderMapper.java
@@ -73,7 +73,7 @@ public class OrderMapper {
 	}
 
 	public static String getDeliveryDays(String[] deliveryDays) throws DoesNotExistException {
-		if (deliveryDays == null)
+		if (deliveryDays == null || deliveryDays.length == 0)
 			return null;
 		String[] days = new String[deliveryDays.length];
 		for (int i = 0; i < deliveryDays.length; i++) {
